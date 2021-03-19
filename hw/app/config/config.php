d8887f7106e4695f2fa2ade2b17286032ffc2e1a
Boris Ts. (cborka) <cborka777@yandex.ru>
2021-03-19 17:14:01 +0700

    2021-03-19 конец дня

--- hw/app/config/config.php
+++ hw/app/config/config.php
@@ -10,7 +10,7 @@ defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 return new \Phalcon\Config([
     'database' => [
         'adapter'     => 'Mysql',
-        'host'        => 'localhost',
+        'host'        => '127.0.0.1',
         'username'    => 'bor',
         'password'    => '432',
         'dbname'      => 'myfs',
