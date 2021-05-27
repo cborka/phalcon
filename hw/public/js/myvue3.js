c1e77197cec2b16048f08398945416ca8b76939a
Boris Ts. (cborka) <cborka777@yandex.ru>
2021-05-27 16:52:50 +0700

    2021-05-27 конец рабочего дня, список дел

--- hw/public/js/myvue3.js
+++ hw/public/js/myvue3.js
@@ -1,15 +1,46 @@
 
+
+
 const ToDoList = {
     data() {
+
+        let list = getTodoList();
+
         return {
-            todos: [
-                {text: 'One'},
-                {text: 'Two'}
-            ]
+            todoList: list,
+            newTodo: 'Новое дело'
         }
+    },
+    methods: {
+        addTodo() {
+//            alert(this.newTodo);
+            let id = 7;
+
+            this.todoList.push({id: id, text: this.newTodo})
+        }
+
     }
 };
 
 const app = Vue.createApp(ToDoList);
+
+app.component(
+    'todo-item',
+    {
+        props: ['todo'],
+        template: `<li>{{todo.text + ', ' + todo.id}} одно дело</li>`
+    }
+);
+
+
 app.mount('#todo-list');
 
+
+//=================================
+
+function getTodoList() {
+    return [
+        {id: 1, text: 'Раз'},
+        {id: 2, text: 'Два'}
+    ];
+}
